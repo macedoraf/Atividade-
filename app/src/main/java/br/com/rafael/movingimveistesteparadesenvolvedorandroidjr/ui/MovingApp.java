70247eecdf230f940a96ea6634f1d3053272d5de
macedoraf <macedo.rafanazareth@gmail.com>
2018-09-22 16:28:04 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/rafael/movingimveistesteparadesenvolvedorandroidjr/ui/MovingApp.java
@@ -0,0 +1,11 @@
+package br.com.rafael.movingimveistesteparadesenvolvedorandroidjr.ui;
+
+import android.app.Application;
+
+public class MovingApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/br/com/rafael/movingimveistesteparadesenvolvedorandroidjr/ui/login/LoginPresenter.java
@@ -0,0 +1,4 @@
+package br.com.rafael.movingimveistesteparadesenvolvedorandroidjr.ui.login;
+
+public interface LoginPresenter {
+}
